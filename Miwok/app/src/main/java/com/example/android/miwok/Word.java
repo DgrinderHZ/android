5686d7afbdc1aaf2506cf747988b98454f45201c
DgrinderHZ <hassanzekkouri@gmail.com>
2021-04-10 02:17:06 +0100

    Finish Day19

--- Miwok/app/src/main/java/com/example/android/miwok/Word.java
+++ Miwok/app/src/main/java/com/example/android/miwok/Word.java
@@ -1,12 +1,21 @@
 package com.example.android.miwok;
 
 public class Word {
+    private static final int NO_IMAGE_PROVIDED = -1;
+
     private String mDefaultTranslation;
     private String mMiwokTranslation;
+    private int mImageResourceId = NO_IMAGE_PROVIDED;
 
     public Word(String DefaultTranslation, String MiwokTranslation) {
         this.mDefaultTranslation = DefaultTranslation;
         this.mMiwokTranslation = MiwokTranslation;
+    }
+
+    public Word(String DefaultTranslation, String MiwokTranslation, int ImageResourceId) {
+        this.mDefaultTranslation = DefaultTranslation;
+        this.mMiwokTranslation = MiwokTranslation;
+        this.mImageResourceId = ImageResourceId;
     }
 
     public String getDefaultTranslation() {
@@ -15,5 +24,13 @@ public class Word {
 
     public String getMiwokTranslation() {
         return mMiwokTranslation;
+    }
+
+    public int getImageResourceId() {
+        return mImageResourceId;
+    }
+
+    public  boolean hasImage(){
+        return this.mImageResourceId != this. NO_IMAGE_PROVIDED;
     }
 }
--- Miwok/app/src/main/java/com/example/android/miwok/WordAdapter.java
+++ Miwok/app/src/main/java/com/example/android/miwok/WordAdapter.java
@@ -6,16 +6,20 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.core.content.ContextCompat;
 
 import java.util.List;
 
 public class WordAdapter extends ArrayAdapter<Word> {
-    public WordAdapter(@NonNull Context context, @NonNull List<Word> objects) {
+    private int mBgColor;
+    public WordAdapter(@NonNull Context context, @NonNull List<Word> objects, int  bgColor) {
         super(context, 0, objects);
+        this.mBgColor = bgColor;
     }
 
     @NonNull
@@ -27,6 +31,11 @@ public class WordAdapter extends ArrayAdapter<Word> {
             listItemView = LayoutInflater.from(getContext()).inflate(
                     R.layout.list_item, parent, false);
         }
+
+        LinearLayout textContainerLinearLayout = listItemView.findViewById(R.id.text_container);
+        // map the color
+        int color = ContextCompat.getColor(getContext(), mBgColor);
+        textContainerLinearLayout.setBackgroundColor(color);
 
         // Get the {@link Word} object located at this position in the list
         Word currentWord = getItem(position);
@@ -42,6 +51,16 @@ public class WordAdapter extends ArrayAdapter<Word> {
         // Get the version name from the current AndroidFlavor object and
         // set this text on the name TextView
         miwokTextView.setText(currentWord.getMiwokTranslation());
+        // Find the ImageView in the list_item.xml layout with the ID placeholder
+        ImageView placeholderImageView =  listItemView.findViewById(R.id.placeholder);
+        if(currentWord.hasImage() == false){
+            placeholderImageView.setVisibility(View.GONE);
+        }else{
+            // Get the version name from the current AndroidFlavor object and
+            // set this text on the name TextView
+            placeholderImageView.setImageResource(currentWord.getImageResourceId());
+        }
+
 
         // Return the whole list item layout (containing 2 TextViews and an ImageView)
         // so that it can be shown in the ListView
